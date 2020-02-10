6278550326523e748a2e073006cb7db50965ab32
Hiroki_Maruta <hiroki4002@yahoo.co.jp>
2020-02-11 00:19:51 +0900

    行の分割

--- separateCSV.cpp
+++ separateCSV.cpp
@@ -8,10 +8,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+// Divide text
+std::vector<std::string> split(std::string str, char del) {
+	int first = 0;
+	int last = (int)str.find_first_of(del);
+
+	std::vector<std::string> result;
+
+	while (first < str.size()) {
+		std::string subStr(str, first, last - first);
+
+		result.push_back(subStr);
+
+		first = last + 1;
+		last = (int)str.find_first_of(del, first);
+
+		if (last == std::string::npos) {
+			last = (int)str.size();
+		}
+	}
+	return result;
+}
 
 int main(){
     std::ifstream csv_file("/Users/marurun/Downloads/TEST4003 江口.CSV");
     std::string line;
+    std::vector<std::string> result;
 
     if(csv_file.fail()){
         std::cerr << "Failure to open CSV file" << std::endl;
@@ -19,9 +43,10 @@ int main(){
     }
 
     // while (getline(csv_file, line)){
-    for(int i=0; i<5; i++){
+    for(int i=0; i<1; i++){
         getline(csv_file, line);
-        std::cout << line << std::endl;
+        result = split(line, ',');
+        for(int j=0; j<result.size(); j++) std::cout << j << ", " << result[j] << std::endl;
     }
 
     return 0;
