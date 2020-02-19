84bd237d2502e553a6980795c01d904fd92b0a38
Hiroki_Maruta <hiroki4002@yahoo.co.jp>
2020-02-19 21:58:35 +0900

    configure.txtの１行目のみ読み込みできるようにした

--- separateCSV.cpp
+++ separateCSV.cpp
@@ -5,10 +5,11 @@
 // reproduced or used in any manner whatsoever.
 // ======================================================================
 
-
 #include <iostream>
 #include <fstream>
 #include <vector>
+
+std::ifstream csv_file;
 
 // Divide text
 std::vector<std::string> split(std::string str, char word){
@@ -32,15 +33,68 @@ std::vector<std::string> split(std::string str, char word){
 	return result;
 }
 
+// configure.txtで条件を設定
+int readConfigFile(std::ifstream* config_file) {
+	std::string str;
+	std::vector<std::string> result;
+    std::cout << "1" << std::endl;
+
+	// // 各種パラメータを取得
+	getline(*config_file, str);
+	result = split(str, ':');
+    std::cout << result[1] << std::endl;
+
+    csv_file.open(result[1]);
+    if(csv_file.fail()){
+        std::cout << "3" << std::endl;
+        std::cerr << "Failed to read the csv file." << std::endl;
+        return -1;
+    }
+    getline(csv_file, str);
+    std::cout << str << std::endl;
+
+    std::cout << "2" << std::endl;
+
+
+	// limitx = std::stof(result[1]);
+	// getline(config_file, str);
+	// result = split(str, '=');
+	// result[1].copy(file_name, 256);
+
+
+	// // Combine path and file name.
+	// strcpy(output_csv_file_name, file_name);
+	// strcpy(jpeg_file_name, file_name);
+	// strcat(path, output_csv_file_name);
+	// strcpy(output_csv_temp_dis, output_csv_file_name);
+	// strcpy(ubh_file_name, path);
+	// strcat(ubh_file_name, ".ubh");
+	// strcpy(csv_file_name, path);
+	// strcat(csv_file_name, ".csv");
+	// strcat(output_csv_file_name, "_result.csv");
+	// strcat(output_csv_temp_dis, "_xy.csv");
+	// strcat(jpeg_file_name, ".jpg");	
+
+	// // The file open as read only
+	// if ((fp = fopen(ubh_file_name, "r")) == 0) {
+	// 	std::cout << "Failed to read the UBH file." << std::endl;
+	// 	return EXIT_FAILURE;
+	// }
+
+	return 1;
+}
+
 int main(int argc, char *argv[]){
-    std::ifstream csv_file("/Users/marurun/Downloads/TEST4003 江口.CSV");
+    std::ifstream config_file("configure.txt");
+	if (config_file.fail()) {
+		std::cerr << "Failed to read the config file." << std::endl;
+		return -1;
+	}
+
     std::string line;
     std::vector<std::string> result;
 
-    if(csv_file.fail()){
-        std::cerr << "Failure to open CSV file" << std::endl;
-        return -1;
-    }
+    readConfigFile(&config_file);
 
     // while (getline(csv_file, line)){
     for(int i=0; i<5; i++){
