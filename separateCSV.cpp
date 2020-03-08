79d30c3ddb312ffc411bb19fc3aa88b21adcb994
Hiroki_Maruta <hiroki4002@yahoo.co.jp>
2020-03-08 20:45:23 +0900

    プロトタイプ宣言を行いメイン関数を一番上に持ってきた

--- separateCSV.cpp
+++ separateCSV.cpp
@@ -1,5 +1,5 @@
 // ======================================================================
-// Copyright c <2020.3.7> Hiroki Maruta. All rights reserved.
+// Copyright c <2020.3.8> Hiroki Maruta. All rights reserved.
 //
 // This source code or any portion thereof must not be  
 // reproduced or used in any manner whatsoever.
@@ -8,6 +8,92 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
+// プロトタイプ宣言
+std::vector<std::string> split(std::string str, char word);
+int readConfigFile(std::ifstream& config_file, std::string& csv_name, int& num, int& skip_num, std::string& cols_name);
+
+
+int main(int argc, char *argv[]){
+    std::ifstream config_file("configure.txt");
+    std::ifstream csv_file;
+    std::ofstream out_csv[256];
+    
+	if (config_file.fail()) {
+		std::cerr << "Failed to read the config file." << std::endl;
+		return -1;
+	}
+
+    int csv_num, skip_num;
+    std::string cols_name, csv_name;
+    std::string line;
+    std::vector<std::string> result;
+
+    // 設定ファイルの読み込み
+    readConfigFile(config_file, csv_name, csv_num, skip_num, cols_name);
+
+    // CSVファイルの読み込み
+    csv_file.open(csv_name);
+    if(csv_file.fail()){
+        std::cerr << "Failed to read the csv file." << std::endl;
+        return -1;
+    }
+
+    char filename[256];
+    for(int i=0; i<csv_num; i++){
+        sprintf(filename, "separated%03d.csv", i);
+        out_csv[i].open(filename);
+    }
+    
+    // CSVファイルを１行ずつ読み込んで，","で分割して一定間隔でCSVファイルに保存
+    int skip_counter = 0, row_counter = 0;
+    while(getline(csv_file, line)){
+        if((row_counter > 3)&&(skip_counter != skip_num)) { // スキップするか判定・実行
+            skip_counter++;
+            continue;
+        }
+
+        int i=0;
+        int min_col=0, max_col=result.size() / csv_num;
+
+        result = split(line, ',');
+        for(int j=0; j<result.size(); j++){
+            if(cols_name == "x"){
+                if(((j%4) == 2) || ((j%4) == 3)) continue; // time,X軸列だけ出力するためY,Z軸をスキップ
+            }else if(cols_name == "y"){
+                if(((j%4) == 1) || ((j%4) == 3)) continue; // time,Y軸列だけ出力
+            }else if(cols_name == "z"){
+                if(((j%4) == 1) || ((j%4) == 2)) continue; // time,Z軸列だけ出力
+            }
+
+            if((j>=min_col)&&(j<=max_col)){
+                out_csv[i] << result[j] << ",";
+                if(max_col==j) {
+                    min_col += result.size() / csv_num;
+                    max_col += result.size() / csv_num;
+                    i++;
+                }
+            }
+        }
+
+        // 改行
+        for(int k=0; k<csv_num; k++){
+            out_csv[k] << std::endl;
+        }
+        skip_counter = 0;
+        row_counter++;
+    }
+
+    // ファイルを閉じる
+    config_file.close();
+    csv_file.close();
+    for(int i=0; i<csv_num; i++){
+        out_csv[i].close();
+    }
+
+    return 0;
+}
+
 
 // Divide text
 std::vector<std::string> split(std::string str, char word){
@@ -59,84 +145,4 @@ int readConfigFile(std::ifstream& config_file, std::string& csv_name, int& num, 
     cols_name = result[1];
 
 	return 1;
-}
-
-int main(int argc, char *argv[]){
-    std::ifstream config_file("configure.txt");
-    std::ifstream csv_file;
-    std::ofstream out_csv[256];
-    
-	if (config_file.fail()) {
-		std::cerr << "Failed to read the config file." << std::endl;
-		return -1;
-	}
-
-    int csv_num, skip_num;
-    std::string cols_name, csv_name;
-    std::string line;
-    std::vector<std::string> result;
-
-    // 設定ファイルの読み込み
-    readConfigFile(config_file, csv_name, csv_num, skip_num, cols_name);
-
-    // CSVファイルの読み込み
-    csv_file.open(csv_name);
-    if(csv_file.fail()){
-        std::cerr << "Failed to read the csv file." << std::endl;
-        return -1;
-    }
-
-    char filename[256];
-    for(int i=0; i<csv_num; i++){
-        sprintf(filename, "separated%03d.csv", i);
-        out_csv[i].open(filename);
-    }
-    
-    // CSVファイルを１行ずつ読み込んで，","で分割して一定間隔でCSVファイルに保存
-    int skip_counter = 0, row_counter = 0;
-    while(getline(csv_file, line)){
-        if((row_counter > 3)&&(skip_counter != skip_num)) { // スキップするか判定・実行
-            skip_counter++;
-            continue;
-        }
-
-        int i=0;
-        int min_col=0, max_col=result.size() / csv_num;
-
-        result = split(line, ',');
-        for(int j=0; j<result.size(); j++){
-            if(cols_name == "x"){
-                if(((j%4) == 2) || ((j%4) == 3)) continue; // time,X軸列だけ出力
-            }else if(cols_name == "y"){
-                if(((j%4) == 1) || ((j%4) == 3)) continue; // time,Y軸列だけ出力
-            }else if(cols_name == "z"){
-                if(((j%4) == 1) || ((j%4) == 2)) continue; // time,Z軸列だけ出力
-            }
-
-            if((j>=min_col)&&(j<=max_col)){
-                out_csv[i] << result[j] << ",";
-                if(max_col==j) {
-                    min_col += result.size() / csv_num;
-                    max_col += result.size() / csv_num;
-                    i++;
-                }
-            }
-        }
-
-        // 改行
-        for(int k=0; k<csv_num; k++){
-            out_csv[k] << std::endl;
-        }
-        skip_counter = 0;
-        row_counter++;
-    }
-
-    // ファイルを閉じる
-    config_file.close();
-    csv_file.close();
-    for(int i=0; i<csv_num; i++){
-        out_csv[i].close();
-    }
-
-    return 0;
 }
